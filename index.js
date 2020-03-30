fc885e0696a7b176ace073308b8d6edffa14774f
walter cardona <cardonacardonawalter31@gmail.com>
2020-03-29 19:11:14 -0500

    ejemplo node

---
+++ index.js
@@ -0,0 +1,17 @@
+function saludo() {
+    console.log("bienvenido nodejs");
+}
+
+function sumOperation() {
+    var resultado = 20 +30;
+    return  resultado;
+}
+
+function printResultado() {
+
+    console.log("el resultado es: "+ sumOperation());
+}
+
+saludo();
+//sumOperation();
+printResultado();
